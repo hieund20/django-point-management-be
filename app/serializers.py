967beab3e90627ec614626fa0163d4b5858da2c9
hieudnguyen <hieudnguyen@meu-solutions.com>
2023-03-12 23:07:59 +0700

    fix password

--- app/serializers.py
+++ app/serializers.py
@@ -12,15 +12,13 @@ class UserSerializer(ModelSerializer):
     # courses = CourseSerializer(many=True)
     class Meta:
         model = User
-        fields = ['id', 'username', 'first_name', 'last_name', 'email', 'is_staff', 'is_superuser', 'className', 'date_joined']
-        # extra_kwargs = {
-        #     'password': {'write_only': 'true'}
-        # }
+        fields = ['id', 'username', 'first_name', 'last_name', 'password', 'email', 'is_staff', 'is_superuser', 'className', 'date_joined']
+        extra_kwargs = {
+            'password': {'write_only': 'true'}
+        }
 
     def create(self, validated_data):
         user = User(**validated_data)
-        # user.first_name = validated_data['first_name']
-        # user.last_name = validated_data['last_name']
         user.set_password(validated_data['password'])
         user.save()
 
