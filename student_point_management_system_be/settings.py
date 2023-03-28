7534fd4e69202240535d482c33f82dc8cbbb4e9a
hieudnguyen <hieudnguyen@meu-solutions.com>
2023-03-28 21:32:23 +0700

    update

--- student_point_management_system_be/settings.py
+++ student_point_management_system_be/settings.py
@@ -185,6 +185,6 @@ EMAIL_PORT = 587
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+MEDIA_ROOT = '%s/app/static/' % BASE_DIR
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 django_heroku.settings(locals())
